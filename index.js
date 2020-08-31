d7ece6ccb910f6540658068b525dd2b8caf66df8
gkarthiks <github.gkarthiks@gmail.com>
2020-09-01 02:48:41 +0530

    enhancement

--- index.js
+++ index.js
@@ -12,13 +12,25 @@ Date.prototype.ddmmyyyy = function() {
            ].join('-');
 };
 
+Date.prototype.mmddyyyy = function() {
+    var mm = this.getMonth() + 1;
+    var dd = this.getDate();
+  
+    return [(mm>9 ? '' : '0') + mm,
+            (dd>9 ? '' : '0') + dd,
+            this.getFullYear()
+           ].join('-');
+};
+
 try {
     
     var startingParseSymbol = core.getInput("starting-parse-symbol");
     var issueContext = github.context.payload.issue.body;
+    var issueName = github.context.payload.issue.title;
     var fileNameFormat = core.getInput("file-name-format");
     var pathToSave = core.getInput("path-to-save");
     var fileNameExtension = core.getInput("file-name-extension");
+    var fileNameDate, completeFileName;
 
     console.log(`
     The symbol declared for parsing is ${startingParseSymbol}
@@ -33,7 +45,16 @@ try {
     if (scheduledTime === "") {
         console.log("Scheduled time is null, creating the file name with the specified format.")
         var date = new Date();
-        console.log(date.ddmmyyyy());
+        if (fileNameFormat === "dd-mm-yyyy") {
+            console.log(date.ddmmyyyy());
+            fileNameDate = date.ddmmyyyy();
+        } else if (fileNameFormat === "mm-dd-yyyy") {
+            console.log(date.mmddyyyy());
+            fileNameDate = date.mmddyyyy();
+        }
+        completeFileName = fileNameDate+issueName+"."+fileNameExtension
+
+        console.log(`file name tio be saved is ${completeFileName}`)
     }
     
 } catch (error) {
