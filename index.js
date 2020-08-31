3c16fc5ddc0841b7b1432a792bf0a838862f98df
gkarthiks <github.gkarthiks@gmail.com>
2020-08-31 23:17:37 +0530

    enhancement

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ try {
   var startingParseSymbol = core.getInput("starting-parse-symbol")
   var eventPayload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The starting parsing symbol declared======> ${startingParseSymbol}!`);
+
+  console.log(`the event payload is =====>>>>>. ${eventPayload}`);
+
 } catch (error) {
   core.setFailed(error.message);
 }
