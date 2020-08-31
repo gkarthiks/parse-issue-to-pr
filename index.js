1ee7da08f5ea3c3a1703ca7640ec12d6654c27c2
gkarthiks <github.gkarthiks@gmail.com>
2020-09-01 01:44:26 +0530

    enhancement

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const github = require('@actions/github');
 
 
 try {
-  var startingParseSymbol = core.getInput("starting-parse-symbol")
-  var eventPayload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The starting parsing symbol declared======> ${startingParseSymbol}!`);
+  var startingParseSymbol = core.getInput("starting-parse-symbol");
+  var eventPayload = JSON.stringify(github.context.payload, undefined, 2);
+  var issueContext = github.context.payload.issue.body;
 
-  console.log(`the event payload is =====>>>>>. ${eventPayload}`);
+  console.log(`The starting parsing symbol declared======> ${startingParseSymbol}!`);
   
-  console.log(`the event payload is =====>>>>>. ${github.context.payload.issue.body}`);
+  console.log(`the issue context is  =====>>>>>. ${issueContext}`);
 
 } catch (error) {
   core.setFailed(error.message);
