5e3813fd986a2d7988996c2f3144352ad42d295c
gkarthiks <github.gkarthiks@gmail.com>
2020-08-31 23:01:52 +0530

    enhancement

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-  console.log(`The event payload: ${github.issue.context}`);
+  console.log(`The event payload: ${github}`);
 } catch (error) {
   core.setFailed(error.message);
 }
