864c0f407073534d2a4c36e21a8b2ea545378d8a
gkarthiks <github.gkarthiks@gmail.com>
2020-08-31 23:16:09 +0530

    enhancement

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('starting-parse-symbol');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("content", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-  console.log(`The event payload: ${github}`);
+  var startingParseSymbol = core.getInput("starting-parse-symbol")
+  var eventPayload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The starting parsing symbol declared======> ${startingParseSymbol}!`);
 } catch (error) {
   core.setFailed(error.message);
 }
