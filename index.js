bf84527993c6bcb4de15030d54e7b4a7d16ee4df
gkarthiks <github.gkarthiks@gmail.com>
2020-09-01 03:11:50 +0530

    enhancement

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-
+const fs = require('fs');
 
 Date.prototype.ddmmyyyy = function() {
     var mm = this.getMonth() + 1;
